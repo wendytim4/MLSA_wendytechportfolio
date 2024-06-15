e89f20c0b5343b71303af6919f5ec602762293b5
wendytim4 <109157790+wendytim4@users.noreply.github.com>
2024-06-15 21:23:47 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,13 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Wendy Tima Gamvah Pasiah",
+  title: "Software Developer",
+  email: "wendy.gamvah@gmail.com",
+  gitHub: "wendytim4",
+  instagram: "therealwendygamvah",
+  linkedIn: "wendy-pasiah",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
