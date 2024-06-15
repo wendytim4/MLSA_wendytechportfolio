03deb655a43436967dfc522c2e1028dfd528eef9
wendytim4 <109157790+wendytim4@users.noreply.github.com>
2024-06-15 21:56:01 +0000

    customize my portfolio

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -48,7 +48,6 @@ const projectList = [
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
     url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
   },
-
 ];
 
 const Portfolio = () => {
